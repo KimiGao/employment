a74e65e288f1fc73de3ba403836fb22707d90dbe
Kimi <kimigao1986@gmail.com>
2013-08-04 23:26:41 +0800

    initial employment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Employment::Application.config.secret_key_base = 'fd6b12bdffa8193637e21c3c9228f2051b6938e5b835b0d6b324da9796675a4dbdec21f31f8b0bdd6a0c9c442ffae655aa70b71677fc1452e5bbe3c97300837e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Employment::Application.config.session_store :cookie_store, key: '_employment_session'
